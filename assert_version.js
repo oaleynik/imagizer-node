72e87bd24b20355f9641c89a81d09193e1bda52d
Oleg Aleynik <oleg.aleynik@gmail.com>
2019-08-19 21:32:59 +0300

    chore: drop not used files

--- assert_version.js
+++
@@ -1,9 +0,0 @@
-var packageVersion = require('./package').version;
-var distVersion = require('./dist/imagizer-node').VERSION;
-
-if (packageVersion === distVersion) {
-  return 0;
-} else {
-  process.stdout.write("FAIL: package.json and lib/imagizer-node.js versions do not match!\n");
-  return 1;
-}
